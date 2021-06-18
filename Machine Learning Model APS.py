155a4a5b8e47a6bcbb01a0ce2dac096f3c03784a
RodzanIskandar <82149131+RodzanIskandar@users.noreply.github.com>
2021-06-18 23:38:16 +0700

    Upload Machine Learning progress python script

---
+++ Machine Learning Model APS.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
+
+df = pd.read_csv('Train Feature Engineering.csv')
+selected_features = pd.read_csv('selected_features.csv')
+selected_features = selected_features['0'].tolist()
+
+y = df['satisfaction']
+X = df[selected_features]
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=41, test_size=0.2)
+
+# model selection with metrics, the metrics that I use is accuracy and precission in model selection.
+# precission is choose cause I want to minimize the False Positive (not satisfied data classify as satisfied) to get the worst case scenario, so the Airline can do maximum improve in the service
+def model_selection(X_train, y_train, X_test, y_test, models):
+    
+    from sklearn.metrics import accuracy_score, precision_score
+    
+    accuracy_result = []
+    precission_result = []
+    str_models = []
+    
+    for model in models:
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
+        precission = precision_score(y_test, y_pred)
+        accuracy = accuracy_score(y_test, y_pred)       
+        accuracy_result.append(accuracy)
+        precission_result.append(precission)  
+        str_models.append(str(model))
+    
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10,10))
+
+    ax1.plot(accuracy_result)
+    ax1.set_ylabel('accuracy_score')
+
+    ax2.plot(str_models,precission_result)
+    ax2.set_ylabel('precission_result')
+    ax2.set_xticklabels(str_models, rotation=90)
+    plt.tight_layout()
+    
+    return pd.DataFrame({'models':models, 'accuracy':accuracy_result, 'precission':precission_result}) 
+
+# import the models
+from sklearn.linear_model import LogisticRegression, RidgeClassifier
+
+models = [LogisticRegression(), RidgeClassifier(alpha=0.005), ]
